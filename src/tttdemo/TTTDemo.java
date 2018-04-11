d35c21d72f10d2408916c8bab684b7791273dcda
Tommy Haugaard <tha@easv.dk>
2018-04-11 22:34:49 +0200

    Something else

---
+++ src/tttdemo/TTTDemo.java
@@ -0,0 +1,30 @@
+
+package tttdemo;
+
+/**
+ *
+ * @author tha
+ */
+public class TTTDemo {
+    private int x;
+
+    public TTTDemo(int x) {
+        this.x = x;
+    }
+    
+    public TTTDemo() {
+        this.x = 4;
+    }
+    
+    public int calculate1(int n){
+        n = n + 7;
+        return n;
+        
+    }
+    
+     public int calculate2(int n){
+        // 
+        return 89;
+    }
+    
+}
